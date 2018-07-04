b60041ae20791c265d8f7fbcd2dc8b25f58586d1
ed-silva <32685663+ed-silva@users.noreply.github.com>
2018-07-04 14:58:12 -0700

    adding event handlers and state

--- src/Note.js
+++ src/Note.js
@@ -1,17 +1,64 @@
 import React, { Component } from 'react';
 import FaPencil from 'react-icons/lib/fa/pencil';
+import FaTrash from 'react-icons/lib/fa/trash';
+import FaFloppyO from "react-icons/lib/fa/floppy-o";
 
 class Note extends Component {
-  render() {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      editing: false
+    }
+  }
+
+  edit = () => {
+    this.setState({
+      editing: true
+    })
+  }
+  remove = () => {
+    alert('removing note')
+  }
+
+  save = () => {
+    alert('saved!')
+  }
+
+  renderForm = () => {
+    return (
+      <div className="note">
+        <form action="">
+          <textarea/>
+          <button onClick={this.save}><FaFloppyO/></button>
+        </form>
+      </div>
+    )
+  }
+
+  renderDisplay = () => {
     return (
       <div className="note">
         <p>Learn React</p>
         <span>
-          <button>Edit</button>
-          <button>Remove</button>
+          <button
+            onClick={this.edit}
+            id='edit'>
+            <FaPencil />
+          </button>
+          
+          <button
+            onClick={this.remove}
+            id='remove'>
+            <FaTrash />
+          </button>
         </span>
       </div>
     )
+  }
+
+  render() {
+    return this.state.editing ? this.renderForm() : this.renderDisplay();
   }
 }
 
